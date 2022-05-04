182ccab715145515ac1758e5a4ab7398a4a25a1a
Dietmar Zabel <dietmar.zabel@otto.de>
2022-05-04 17:42:58 +0200

    3.13 add padding

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="about pa-6">
     <h1>About Vuetify Todo</h1>
 
     <p> Ipsum Generator. Generate Lorem Ipsum placeholder text. Select the number of characters, words, sentences or paragraphs, and hit generate! Generated Lorem Ipsum. Copy. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea</p>
