8d40bce621f0cc4666f1847f9b32129274308152
Dietmar Zabel <dietmar.zabel@otto.de>
2022-05-04 19:26:03 +0200

    4.19 Mark task as done

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -9,13 +9,17 @@
       <div v-for="task in tasks"
            :key="task.id"
       >
-        <v-list-item>
-          <template v-slot:default="{ active }">
+        <v-list-item @click="doneTask(task.id)"
+                     :class="{'blue lighten-5' : task.done}"
+        >
+          <template>
             <v-list-item-action>
-              <v-checkbox :input-value="active"></v-checkbox>
+              <v-checkbox :input-value="task.done"></v-checkbox>
             </v-list-item-action>
             <v-list-item-content>
-              <v-list-item-title>{{ task.title }}</v-list-item-title>
+              <v-list-item-title
+                  :class="{'text-decoration-line-through' : task.done}"
+              >{{ task.title }}</v-list-item-title>
             </v-list-item-content>
           </template>
         </v-list-item>
@@ -34,17 +38,26 @@
         tasks: [
           {
             id: 1,
-            title: 'Wake up'
+            title: 'Wake up',
+            done: false
           },
           {
             id: 2,
-            title: 'Get bananas'
+            title: 'Get bananas',
+            done: false
           },
           {
             id: 3,
-            title: 'Eat bananas'
+            title: 'Eat bananas',
+            done: false
           }
         ]
+      }
+    },
+    methods: {
+      doneTask(id) {
+        let task = this.tasks.filter(task => task.id === id)[0]
+        task.done = !task.done
       }
     }
   }
