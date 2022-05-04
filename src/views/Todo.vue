c9bbcb2a9f479ba09fb79ba88ea0fe7beff67306
Dietmar Zabel <dietmar.zabel@otto.de>
2022-05-04 18:58:01 +0200

    4.18 Dummy List with dynamic js

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -2,19 +2,25 @@
   <div class="home">
 
     <v-list
+        class="pt-0"
         flat
     >
-      <v-list-item>
-        <template v-slot:default="{ active }">
-          <v-list-item-action>
-            <v-checkbox :input-value="active"></v-checkbox>
-          </v-list-item-action>
 
-          <v-list-item-content>
-            <v-list-item-title>Wake up</v-list-item-title>
-          </v-list-item-content>
-        </template>
-      </v-list-item>
+      <div v-for="task in tasks"
+           :key="task.id"
+      >
+        <v-list-item>
+          <template v-slot:default="{ active }">
+            <v-list-item-action>
+              <v-checkbox :input-value="active"></v-checkbox>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>{{ task.title }}</v-list-item-title>
+            </v-list-item-content>
+          </template>
+        </v-list-item>
+        <v-divider></v-divider>
+      </div>
 
     </v-list>
   </div>
@@ -23,6 +29,23 @@
 <script>
   export default {
     name: 'Home',
-
+    data() {
+      return {
+        tasks: [
+          {
+            id: 1,
+            title: 'Wake up'
+          },
+          {
+            id: 2,
+            title: 'Get bananas'
+          },
+          {
+            id: 3,
+            title: 'Eat bananas'
+          }
+        ]
+      }
+    }
   }
 </script>
