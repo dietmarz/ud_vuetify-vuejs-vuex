7db98f978f2435bc88f16fb66863e0df34076793
Dietmar Zabel <dietmar.zabel@otto.de>
2022-06-06 15:12:03 +0200

    6.31 Create Add Task Field Child Component

--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,18 +1,7 @@
 <template>
   <div class="home">
 
-    <v-text-field
-        v-model="newTaskTitle"
-        @click:append="addTask()"
-        @keyup.enter="addTask()"
-        class="pa-3"
-        outlined
-        label="Add Task"
-        append-icon="mdi-plus"
-        hide-details
-        clearable
-    ></v-text-field>
-
+    <field-add-task />
     <v-list
         v-if="$store.state.tasks.length"
         class="pt-0"
@@ -67,17 +56,8 @@
 <script>
   export default {
     name: 'Home',
-    data() {
-      return {
-        newTaskTitle: "",
-        tasks: []
-      }
-    },
-    methods: {
-      addTask() {
-        this.$store.commit('addTask', this.newTaskTitle);
-        this.newTaskTitle = '';
-      }
+    components: {
+      'field-add-task': require('@/components/Todo/FieldAddTask.vue').default
     }
   }
 </script>
