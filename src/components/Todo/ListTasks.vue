73c1b5db8cfd2139dcb25252f073e3f62b282f7b
Dietmar Zabel <dietmar.zabel@otto.de>
2022-06-06 16:46:52 +0200

    6.33 Task child component with props argument

--- src/components/Todo/ListTasks.vue
+++ src/components/Todo/ListTasks.vue
@@ -4,33 +4,11 @@
       flat
   >
 
-    <div v-for="task in $store.state.tasks"
-         :key="task.id"
-    >
-      <v-list-item @click="$store.commit('doneTask', task.id )"
-                   :class="{'blue lighten-5' : task.done}"
-      >
-        <template>
-          <v-list-item-action>
-            <v-checkbox :input-value="task.done"></v-checkbox>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title
-                :class="{'text-decoration-line-through' : task.done}"
-            >{{ task.title }}</v-list-item-title>
-          </v-list-item-content>
-
-          <v-list-item-action>
-            <v-btn icon
-                   @click.stop="$store.commit('deleteTask', task.id)">
-              <v-icon color="primary lighten-1">mdi-trash-can-outline</v-icon>
-            </v-btn>
-          </v-list-item-action>
-
-        </template>
-      </v-list-item>
-      <v-divider></v-divider>
-    </div>
+    <task
+        v-for="task in $store.state.tasks"
+        :key="task.id"
+        :task="task"
+    />
 
   </v-list>
 
@@ -38,7 +16,9 @@
 
 <script>
 export default {
-  name: "ListTasks"
+  components: {
+    'task': require('@/components/Todo/Task.vue').default
+  }
 }
 </script>
 
