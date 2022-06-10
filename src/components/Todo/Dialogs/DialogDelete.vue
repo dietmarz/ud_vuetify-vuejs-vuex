807dad0a7598eb42bf27c0a283cb50d46575c4de
Dietmar Zabel <dietmar.zabel@otto.de>
2022-06-10 08:38:51 +0200

    8.43 Adding delete Dialog permanently

---
+++ src/components/Todo/Dialogs/DialogDelete.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-dialog
+      :value="true"
+      persistent
+      max-width="290"
+  >
+
+    <v-card>
+      <v-card-title class="text-h5">
+        Delete task?
+      </v-card-title>
+      <v-card-text>Are you sure you wanna delete this task?</v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+            text
+            @click="dialog = false"
+        >
+          No
+        </v-btn>
+        <v-btn
+            color="primary darken-1"
+            text
+            @click="dialog = false"
+        >
+          Yes
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "DialogDelete.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
